f0dbfc644296363f76455953d391b7bbc0016903
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-02 15:52:31 -0700

    Completed lesson on strings.

---
+++ 01fundamentals/ZTM.DotNet/Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿string message = "Hello World";
+Console.WriteLine(message);
+
+// empty string
+string emptyMessage = string.Empty;
+
+string stringWithDoubleQuote = "This is a string with a double quote: \" ";
+Console.WriteLine(stringWithDoubleQuote);
+
+string filepath = "C:\\temp";
+Console.WriteLine(filepath);
+
+string filepath2 = @"C:\temp";
+Console.WriteLine(filepath2);
+
+// string length
+Console.WriteLine(filepath.Length);
+
+// string concatenation
+string firstName = "Jonathan";
+string lastName = "Vos";
+
+string fullName = firstName + " " + lastName;
+Console.WriteLine(fullName);
+
+// string interpolation
+string fullNameInterpolated = $"{firstName} {lastName}";
+Console.WriteLine(fullNameInterpolated);
+Console.WriteLine($"The result is: {5 * 7}");
+
+// substrings
+string first5Characters = fullName.Substring(0, 3);
+Console.WriteLine(first5Characters);
