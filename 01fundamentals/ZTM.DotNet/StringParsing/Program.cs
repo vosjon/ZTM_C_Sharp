f453cbb82f47b1526f2c57d7984e5d52ffb5309c
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-02 17:45:38 -0700

    Completed string parsing lesson.

---
+++ 01fundamentals/ZTM.DotNet/StringParsing/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+string luckyNumber = "16";
+
+int parsedLuckyNumber = int.Parse(luckyNumber);
+Console.WriteLine(parsedLuckyNumber.GetType());
+
+CultureInfo cultureInfo = new CultureInfo("de-DE");
+double tempurature = double.Parse("30,7", cultureInfo);
+Console.WriteLine(tempurature);
