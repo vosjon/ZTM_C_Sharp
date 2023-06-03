e62abf63018196d25f3cfa67fa56d4fae7ee9697
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-02 22:01:17 -0700

    Completed DateTime lesson.

---
+++ 01fundamentals/ZTM.DotNet/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+DateTime dateOfBirth = new DateTime(1987, 10, 10);
+Console.WriteLine(dateOfBirth.DayOfWeek);
+
+// Computed Values
+DateTime today = DateTime.Today;
+DateTime now = DateTime.Now;
+DateTime utcNow = DateTime.UtcNow;
+
+// DateTime Parsing
+DateTime localDate = DateTime.Parse("10.10.1987");
+DateTime usDate = DateTime.Parse("10/10/1987", new CultureInfo("en-US"));
+
+Console.WriteLine($"localDate: {localDate}, usDate: {usDate}");
+
+// DateTime Formatting
+Console.WriteLine(dateOfBirth.ToString());
+Console.WriteLine(dateOfBirth.ToString(new CultureInfo("en-US")));
+Console.WriteLine(dateOfBirth.ToString("yyyy-MM-dd"));
