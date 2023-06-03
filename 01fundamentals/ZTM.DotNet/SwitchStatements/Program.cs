b935a4437043dd50dff6bd7e1ab976f60ebd0659
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-03 16:13:12 -0700

    Completed the switch statement lesson.

---
+++ 01fundamentals/ZTM.DotNet/SwitchStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Enter your age: ");
+string? input = Console.ReadLine();
+int age = int.Parse(input);
+
+switch (age)
+{
+    case < 10:
+        Console.WriteLine("You're a child.");
+        break;
+    case < 20:
+        Console.WriteLine("You're a teenager.");
+        break;
+    case < 30:
+        Console.WriteLine("You're a young adult.");
+        break;
+    case < 40:
+        Console.WriteLine("You're an adult.");
+        break;
+    case < 50:
+        Console.WriteLine("You're still an adult.");
+        break;
+    default:
+        Console.WriteLine("You're a wise person.");
+        break;
+}
+
+switch (age)
+{
+    case 10:
+    case 20:
+    case 30:
+    case 40:
+    case 50:
+    case 60:
+        Console.WriteLine("You just had a round birthday.");
+        break;
+    default:
+        Console.WriteLine("You just had a normal birthday.");
+        break;
+}
