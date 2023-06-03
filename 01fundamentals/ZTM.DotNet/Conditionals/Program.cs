c650867158c56a8643efdd8f01291bd159dcfaf0
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-03 15:58:52 -0700

    Completed conditionals lesson.

---
+++ 01fundamentals/ZTM.DotNet/Conditionals/Program.cs
@@ -0,0 +1,39 @@
+﻿// if
+int myLuckyNumber = 16;
+
+if (myLuckyNumber == 16) 
+{
+    Console.WriteLine("You're lucky!");
+}
+
+Console.WriteLine("Conditionals are cool!");
+
+// if - else
+if (myLuckyNumber < 16) 
+{
+    Console.WriteLine("Your lucky number is lower than 16.");
+}
+else
+{
+    Console.WriteLine("Your lucky number is 16 or higher.");
+}
+
+// if - else if - else
+string name = "Jonathan";
+
+if (name == "Jonathan")
+{
+    Console.WriteLine("Hi, Jonathan!");
+}
+else if (name == "Cladio")
+{
+    Console.WriteLine("Hi, Cladio!");
+}
+else
+{
+    Console.WriteLine("Hi, nice to meet you!");
+}
+
+int version = 4;
+string productVersion = version == 4 ? "4.0" : "3.0";
+Console.WriteLine(productVersion);
