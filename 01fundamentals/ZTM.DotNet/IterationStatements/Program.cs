042c8f305dcbfbdce51e75e1e1ce1e7776df9c8a
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-03 16:32:45 -0700

    Completed interation statements lesson.

---
+++ 01fundamentals/ZTM.DotNet/IterationStatements/Program.cs
@@ -0,0 +1,21 @@
+﻿// while
+int a = 1;
+while (a < 5)
+{
+    Console.WriteLine($"while: {a}");
+    a++;
+}
+
+// do-while
+int b = 1;
+do
+{
+    Console.WriteLine($"do-while: {b}");
+    b++;
+} while (b < 3);
+
+// for
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine($"for: {i}");
+}
