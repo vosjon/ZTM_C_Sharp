0ff3b1a869ea185b07049a01d8ccd3d56026e6c2
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-03 21:37:07 -0700

    Completed the basic variable scope lesson.

---
+++ 01fundamentals/ZTM.DotNet/BasicVariableScopes/Program.cs
@@ -0,0 +1,24 @@
+﻿int age = 31;
+
+if (age >= 27 && age <= 43)
+{
+    string generation = "Millenial";
+    Console.WriteLine($"You're a {generation} because you are {age} years old.");
+}
+else
+{
+    string generation = "Unknown";
+    Console.WriteLine($"Your generation is: {generation}");
+}
+
+// Generation cannot be accessed outside the scope of the if else block.
+//Console.WriteLine(generation);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+// Here is another example of the variable being out of scope.
+// i cannot be accessed outside of the for loop.
+//Console.WriteLine(i);
