82cfdcc41c0c60203a051cc52143cfcbca2fb1c5
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-04 13:59:16 -0700

    Completed the final project for the fundamentals section.

---
+++ 01fundamentals/ZTM.DotNetProjectOne/ConsoleBasedCalculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedCalculator
+{
+    public class Calculator
+    {
+        
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static void DisplayTitle()
+        {
+            Console.WriteLine(@"
+
+    __   ____  _         __  __ __  _       ____  ______   ___   ____  
+   /  ] /    T| T       /  ]|  T  T| T     /    T|      T /   \ |    \ 
+  /  / Y  o  || |      /  / |  |  || |    Y  o  ||      |Y     Y|  D  )
+ /  /  |     || l___  /  /  |  |  || l___ |     |l_j  l_j|  O  ||    / 
+/   \_ |  _  ||     T/   \_ |  :  ||     T|  _  |  |  |  |     ||    \ 
+\     ||  |  ||     |\     |l     ||     ||  |  |  |  |  l     !|  .  Y
+ \____jl__j__jl_____j \____j \__,_jl_____jl__j__j  l__j   \___/ l__j\_j
+                                                                       
+
+");
+        }
+    }
+}
--- 01fundamentals/ZTM.DotNetProjectOne/ConsoleBasedCalculator/Program.cs
+++ 01fundamentals/ZTM.DotNetProjectOne/ConsoleBasedCalculator/Program.cs
@@ -1,2 +1,67 @@
 ﻿// Final project for the C# Fundamentals section of the Zero to Mastery C#/.NET Bootcamp
 // Console Based Calculator
+
+using ConsoleBasedCalculator;
+
+Console.WriteLine("-----------------------Welcome to the Console---------------------");
+Calculator.DisplayTitle();
+
+bool keepGoing = true;
+
+while (keepGoing)
+{
+    Console.WriteLine(@"
+
+Enter the number for the choice you would like to make:
+
+1 - Add
+2 - Subtract
+3 - Multiply
+4 - Divide
+");
+    Console.Write("Number: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nEnter number 1: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("\nEnter number 2: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            int addedNum = Calculator.Add(num1, num2);
+            Console.WriteLine(addedNum);
+            break;
+        case 2:
+            int subtractedNum = Calculator.Subtract(num1, num2);
+            Console.WriteLine(subtractedNum);
+            break;
+        case 3:
+            int multipliedNum = Calculator.Multiply(num1, num2);
+            Console.WriteLine(multipliedNum);
+            break;
+        case 4:
+            double dividedNum = Calculator.Divide((double)num1, (double)num2);
+            Console.WriteLine(dividedNum);
+            break;
+        default:
+            Console.WriteLine("You didn't put in the correct number.");
+            break;
+    }
+
+    Console.Write("Would you like to make another calculation? yes or no: ");
+    string again = Console.ReadLine().ToLower();
+    if (again == "no")
+    {
+        keepGoing = false;
+    }
+    else
+    {
+        Console.Clear();
+        continue;
+    }
+}
+
+Console.WriteLine("Thanks for using the calculator!");
