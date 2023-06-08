076bd8f80e84284ce90847638b5ea5107450532e
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-07 18:02:33 -0700

    Finished all OOP lessons up to the first exercise.

--- 02oop/ZTM.DotNet/BasicClass/Program.cs
+++ 02oop/ZTM.DotNet/BasicClass/Program.cs
@@ -4,6 +4,15 @@ Person person2 = new Person("Trish");
 Console.WriteLine(person1.Name);
 Console.WriteLine(person2.Name);
 
+person1.PrintGreeting();
+person2.PrintGreeting();
+
+string greeting = person1.GetGreeting();
+Console.WriteLine(greeting);
+
+Math math = new Math();
+int result = math.Sum(14, 16);
+Console.WriteLine(result);
 
 class Person
 {
@@ -12,4 +21,22 @@ class Person
         Name = name;
     }
     public string Name { get; private set; }
+
+    public void PrintGreeting()
+    {
+        Console.WriteLine($"Hello, {Name}.");
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, {Name}.";
+    }
+}
+
+public class Math
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
 }
