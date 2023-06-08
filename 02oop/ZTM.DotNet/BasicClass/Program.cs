31b488190ed087850795a063d4a70f3e3a0209b2
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-07 17:48:23 -0700

    Completed the basic class lessons in the C# course.

---
+++ 02oop/ZTM.DotNet/BasicClass/Program.cs
@@ -0,0 +1,15 @@
+﻿Person person1 = new Person("Jonathan");
+Person person2 = new Person("Trish");
+
+Console.WriteLine(person1.Name);
+Console.WriteLine(person2.Name);
+
+
+class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; private set; }
+}
