f0caf910082071a033d2b5859bfb3a065dff39ed
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-09 06:25:16 -0700

    Completed the Inheritance lesson.

---
+++ 02oop/ZTM.DotNet/Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿Person person1 = new Person("James");
+Employee employee2 = new Employee("Jessica", "CEO");
+
+Console.WriteLine($"{person1.Name}: {person1.GetId()}");
+Console.WriteLine($"{employee2.Name}: {employee2.GetId()}");
+Console.WriteLine($"Employee: {employee2.Name} Salary: {employee2.GetSalary()}");
+
+public interface ISalary
+{
+    decimal GetSalary();
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public virtual string GetId()
+    {
+        return Name.ToLower();
+    }
+}
+
+public class Employee : Person, ISalary
+{
+    public string Role { get; set; }
+    public Employee(string name, string role) : base(name)
+    {
+        Role = role;
+    }
+    public override string GetId()
+    {
+        return $"{Role.ToLower()}-{base.GetId()}";
+    }
+
+    public decimal GetSalary()
+    {
+        return 2000;
+    }
+}
