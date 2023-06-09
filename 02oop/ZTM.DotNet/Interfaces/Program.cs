c613ff3ab2c166018067287c1c01908d1bf597b6
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-08 17:40:01 -0700

    Completed the Interfaces lesson.

---
+++ 02oop/ZTM.DotNet/Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿ICar telsa = new TeslaModelS();
+ICar nissan = new NissanMicra();
+
+Console.WriteLine($"{telsa.GetModel()}: {telsa.GetTopSpeed()} km/h");
+Console.WriteLine($"{nissan.GetModel()}: {nissan.GetTopSpeed()} km/h");
+
+
+public interface ICar
+{
+    string GetModel();
+    int GetTopSpeed();
+}
+
+public class TeslaModelS : ICar
+{
+    public string GetModel()
+    {
+        return "Tesla Model S";
+    }
+    public int GetTopSpeed()
+    {
+        return 282;
+    }
+}
+
+public class NissanMicra : ICar
+{
+    public string GetModel()
+    {
+        return "Nissan Micra";
+    }
+    public int GetTopSpeed()
+    {
+        return 186;
+    }
+}
