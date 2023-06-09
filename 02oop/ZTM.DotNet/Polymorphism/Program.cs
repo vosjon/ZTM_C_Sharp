31d120bc1ec52c5d20e0f56a25d68b1abcbaf7ab
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-09 06:35:13 -0700

    Completed the polymorphism lesson.

---
+++ 02oop/ZTM.DotNet/Polymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿Person person = new Person();
+Employee employee = new Employee();
+Customer customer = new Customer();
+
+person.Greeting();
+employee.Greeting();
+customer.Greeting();
+
+public class Person
+{
+    public virtual void Greeting()
+    {
+        Console.WriteLine("Hi, I'm a regular person.");
+    }
+}
+
+public class Employee : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hi, I'm an employee.");
+    }
+}
+
+public class Customer : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hi, I'm a customer.");
+    }
+}
