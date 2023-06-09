a233edec5f9f6828e01bc76f000376ba793e84de
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-08 17:26:56 -0700

    Completed the Static members lesson.

---
+++ 02oop/ZTM.DotNet/StaticMembers/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine(Math.Divide(6, 3));
+
+int result = Math.Sum(99, 1);
+Console.WriteLine(result);
+
+Console.WriteLine(Math.PI);
+
+class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; private set; }
+
+    public void PrintGreeting()
+    {
+        Console.WriteLine($"Hello, {Name}.");
+    }
+
+    public string GetGreeting()
+    {
+        return $"Hello, {Name}.";
+    }
+}
+
+public static class Math
+{
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static double PI
+    {
+        get { return 3.14d; }
+    }
+    public static double Divide(double a, double b) => a / b;
+}
