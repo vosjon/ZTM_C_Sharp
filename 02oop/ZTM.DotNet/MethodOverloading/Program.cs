a86f2f39c9f54205e5ee31cd930a48225b7f6fcc
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-09 06:03:33 -0700

    Completed the method overloading lesson.

---
+++ 02oop/ZTM.DotNet/MethodOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿Account account1 = new Account("Paul");
+Account account2 = new Account("Peter", 100);
+
+Console.WriteLine(account1.Owner + " " + account1.Balance);
+Console.WriteLine(account2.Owner + " " + account2.Balance);
+
+public class Account
+{
+    public decimal Balance { get; set; }
+    public string Owner { get; set; }
+
+    public Account(string owner)
+    {
+        Owner = owner;
+        Balance = 0;
+    }
+
+    public Account(string owner, decimal balance)
+    {
+        Owner = owner;
+        Balance = balance;
+    }
+}
