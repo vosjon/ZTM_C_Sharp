7cc264896307472031f7bf424e1dda5c850ece92
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-11 16:35:02 -0700

    Completed the const lesson.

---
+++ 03variabletypes/ZTM.DotNet/Const/Program.cs
@@ -0,0 +1,11 @@
+﻿const int age = 35;
+// age = 32;
+Console.WriteLine(age);
+
+public class Person
+{
+    const int Age = 21;
+    const string ProductName = "C# Course";
+    const string Version = "2";
+    const string FullProductName = $"{ProductName} {Version}";
+}
