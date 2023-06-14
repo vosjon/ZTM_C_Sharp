c2f9349f04cd576e2b36c7f5ad51d70590d79202
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-13 19:48:08 -0700

    Completed the Structs lesson.

---
+++ 03variabletypes/ZTM.DotNet/Structs/Program.cs
@@ -0,0 +1,24 @@
+﻿var retangle = new Rectangle(200, 300);
+var immutableRectangle = new ImmutableRectangle(200, 300);
+
+public struct Rectangle
+{
+    public Rectangle(double width, double height)
+    {
+        Width = width; 
+        Height = height;
+    }
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public readonly struct ImmutableRectangle
+{
+    public ImmutableRectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
