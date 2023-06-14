ff461760bd9c657a99202cc0aa6f7549bb4b4c34
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-13 20:03:41 -0700

    Completed the method arguments lesson.

---
+++ 03variabletypes/ZTM.DotNet/MethodArguments/Program.cs
@@ -0,0 +1,26 @@
+﻿var rectangle = new Rectangle(200, 300);
+Console.WriteLine($"rectangle height: {rectangle.Height}");
+
+MethodParameters.ChangeHeight(ref rectangle);
+Console.WriteLine($"rectangle height: {rectangle.Height}");
+
+
+public struct Rectangle
+{
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public class MethodParameters
+{
+    public static void ChangeHeight(ref Rectangle rectangle)
+    {
+        rectangle.Height = 500;
+        Console.WriteLine($"rectangle height in Method: {rectangle.Height}");
+    }
+}
