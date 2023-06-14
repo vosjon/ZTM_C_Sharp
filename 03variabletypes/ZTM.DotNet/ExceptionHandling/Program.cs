a992eec853de49aa355ec83884b0510b0e383c2e
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-13 20:36:32 -0700

    Completed the exception handling lesson.

---
+++ 03variabletypes/ZTM.DotNet/ExceptionHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿var content = "";
+try
+{
+    content = File.ReadAllText("doc.txt");
+    Console.WriteLine("The file has been read.");
+}
+catch (FileNotFoundException exception)
+{
+    Console.WriteLine($"ERROR: The file '{exception.FileName}' could not be found.");
+}
+finally
+{
+    Console.WriteLine($"The content length is: {content.Length}");
+}
