6e71216550a93b138cdb93fd9b3ed106b83e96df
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-15 20:44:02 -0700

    Completed the queue lesson.

---
+++ 04arraysandcollections/ZTM.DotNet/Queue/Program.cs
@@ -0,0 +1,11 @@
+﻿var messages = new Queue<string>();
+
+messages.Enqueue(".NET is awesome!");
+messages.Enqueue("C# is a great programming language.");
+messages.Enqueue("You are a great student!");
+
+while (messages.Count > 0)
+{
+    var message = messages.Dequeue();
+    Console.WriteLine(message);
+}
