d71e004f5493da3761372d9d9500cf29e42ea892
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-15 20:27:26 -0700

    Completed the List lesson.

---
+++ 04arraysandcollections/ZTM.DotNet/List/Program.cs
@@ -0,0 +1,27 @@
+﻿var numbers = new List<int>();
+var day = 22;
+numbers.Add(day);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"Number: {number}");
+}
+
+var persons = new List<Person>();
+var person1 = new Person("Jonathan");
+persons.Add(person1);
+
+foreach (var person in persons)
+{
+    Console.WriteLine($"Person: {person.Name}");
+}
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
