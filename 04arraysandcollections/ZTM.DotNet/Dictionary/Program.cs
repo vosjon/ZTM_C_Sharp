26572e204c2fd63f87e09b7d74c48115e7a073f1
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-15 20:58:03 -0700

    Completed the dictionary lesson.

---
+++ 04arraysandcollections/ZTM.DotNet/Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿var grades = new Dictionary<int, string>()
+{
+    { 6, "Perfect" }
+};
+
+grades.Add(5, "Good");
+grades[4] = "Okay";
+grades[3] = "Almost";
+grades[2] = "Bad";
+grades[1] = "Worse";
+
+Console.WriteLine(grades[4]);
+
+foreach  (var grade in grades)
+{
+    Console.WriteLine($"{grade.Key}: {grade.Value}");
+}
