68abcc23bb01fd26d22ba8851782a97ec9c8bf44
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-15 20:37:47 -0700

    Completed the stack lesson.

---
+++ 04arraysandcollections/ZTM.DotNet/Stack/Program.cs
@@ -0,0 +1,17 @@
+﻿var countries = new Stack<string>();
+
+countries.Push("Switzerland");
+countries.Push("Germany");
+countries.Push("Ireland");
+countries.Push("United States of America");
+
+var lastCountry = countries.Pop();
+Console.WriteLine($"The last country visited was: {lastCountry}");
+
+var perviouslyListedCountry = countries.Peek();
+Console.WriteLine($"The previously visited country was: {perviouslyListedCountry}");
+
+foreach (var country in countries)
+{
+    Console.WriteLine(country);
+}
