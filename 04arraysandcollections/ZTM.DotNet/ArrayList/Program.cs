60710b3b09b311274b74d1401bb211bbeaeca403
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-15 20:19:30 -0700

    Completed the arraylist lesson.

---
+++ 04arraysandcollections/ZTM.DotNet/ArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+var arrayList = new ArrayList();
+
+var day = 22;
+arrayList.Add(day);
+
+var person1 = new Person("Jonathan");
+arrayList.Add(person1);
+
+var firstElement = arrayList[0];
+Console.WriteLine($"First Element: {firstElement}");
+
+foreach (var item in arrayList)
+{
+    if (item is Person)
+    {
+        var person = item as Person;
+        if (person != null)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+    else
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
