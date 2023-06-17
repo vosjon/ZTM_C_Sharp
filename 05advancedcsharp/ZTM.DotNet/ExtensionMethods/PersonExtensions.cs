12bc93bcf02077479d1c99d236c7b31ab359f456
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-17 12:35:32 -0700

    Completed the extension method lesson.

---
+++ 05advancedcsharp/ZTM.DotNet/ExtensionMethods/PersonExtensions.cs
@@ -0,0 +1,8 @@
+﻿public static class PersonExtensions
+{
+
+    public static string ToFullName(this Person person)
+    {
+        return $"{person.FirstName} {person.LastName}";
+    }
+}
---
+++ 05advancedcsharp/ZTM.DotNet/ExtensionMethods/Program.cs
@@ -0,0 +1,36 @@
+﻿var person = new Person("Jack", "Miller");
+Console.WriteLine(person.ToFullName());
+Console.WriteLine(person.HasID());
+
+public interface IIdentification
+{
+    int Id { get;  }
+}
+
+public static class IdentificationExtensions
+{
+    public static bool HasID(this IIdentification indentification)
+    {
+        return indentification.Id > 0;
+    }
+}
+
+public class Person : IIdentification
+{
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public int Id
+    {
+        get
+        {
+            return 7;
+        }
+    }
+}
