568843bd74237b3385c9f1d168a1e5d895f2dab1
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-17 11:34:12 -0700

    Completed the Named Arguments section from the advanced section

---
+++ 05advancedcsharp/ZTM.DotNet/NamedArguments/Program.cs
@@ -0,0 +1,17 @@
+﻿var person1 = new Person("Jack", 24, 19);
+var person2 = new Person(name: "Jack", age: 24, luckyNumber: 19);
+var person3 = new Person("Jack", age: 24, luckyNumber: 19);
+
+public class Person
+{
+    public Person(string name, int age, int luckyNumber)
+    {
+        Name = name;
+        Age = age;
+        LuckyNumber = luckyNumber;
+    }
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int LuckyNumber { get; set; }
+}
