fb2d57933651e22d535dffeff530f2caea3de74f
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-19 16:03:09 -0700

    Completed the params keyword lesson.

---
+++ 05advancedcsharp/ZTM.DotNet/ParamsKeyword/Program.cs
@@ -0,0 +1,14 @@
+﻿var numberPrinter = new NumberPrinter();
+numberPrinter.PrintNumbers(new int[] { 1, 2, 3 });
+numberPrinter.PrintNumbers(4, 5, 6, 7);
+
+public class NumberPrinter
+{
+    public void PrintNumbers(params int[] numbers)
+    {
+        foreach (var number in numbers)
+        {
+            Console.WriteLine($"{number}");
+        }
+    }
+}
