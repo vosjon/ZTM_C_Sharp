0484e988f371c299607037f0fef95d14e33a9b71
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-20 21:06:01 -0700

    Completed all lessons from the advanced C# section.

---
+++ 05advancedcsharp/ZTM.DotNet/ClassExceptionTypes/InvalidUsernameException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptionTypes;
+
+internal class InvalidUsernameException : Exception
+{
+    public string? Username { get; set; }
+    public InvalidUsernameException(string? username) : base($"The username '{username}' is invalid.")
+    {
+        Username = username;
+    }
+
+    public InvalidUsernameException(string? username, string? message) : base(message)
+    {
+        Username = username;
+    }
+
+    public InvalidUsernameException(string? username, string? message, Exception? innerException) : base(message, innerException)
+    {
+        Username = username;
+    }
+}
---
+++ 05advancedcsharp/ZTM.DotNet/ClassExceptionTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using CustomExceptionTypes;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter username: ");
+        var userInput = Console.ReadLine();
+
+        try
+        {
+            var username = GetNormalizedUsername(userInput);
+            Console.WriteLine($"Username: {username}");
+        }
+        catch (InvalidUsernameException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static string GetNormalizedUsername(string? username)
+    {
+        if (string.IsNullOrEmpty(username) || username.Length < 6)
+        {
+            throw new InvalidUsernameException(username);
+        }
+
+        return username.ToLower();
+    }
+}
