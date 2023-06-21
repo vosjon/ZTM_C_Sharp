9f197b6645b853f068059e45767a6aff9c37cf75
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-20 18:39:47 -0700

    Completed the nullable reference types lesson.

---
+++ 05advancedcsharp/ZTM.DotNet/NullableReferenceTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿var person1 = new Person("Jasmin");
+Console.WriteLine(person1.Name);
+
+Person? person2 = null;
+if (person2 != null)
+    Console.WriteLine(person2.Name);
+
+Person? person3 = null;
+Console.WriteLine(person3?.Name);
+Console.WriteLine("The end.");
+
+public class Person
+{
+    public Person(string firstName)
+    {
+        Name = firstName;
+    }
+
+    public string Name { get; set; }
+}
