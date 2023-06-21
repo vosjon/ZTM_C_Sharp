233ac45cb74fc969bb5221418afef07358d3158c
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-20 18:47:45 -0700

    Completed the record types lesson.

---
+++ 05advancedcsharp/ZTM.DotNet/RecordTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿var person1 = new Person("Peter", "Parker");
+var person2 = new Person("Peter", "Parker");
+
+if (person1 == person2)
+{
+    Console.WriteLine("Persons are equal");
+}
+
+public record Person(string FirstName, string LastName);
+
+public record Employee(): Person("First", "Last");
