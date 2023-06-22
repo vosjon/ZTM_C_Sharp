3960d61e2675527494212f11473f053b2e5ae783
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-21 21:03:40 -0700

    Completed the LINQ introduction lesson.

---
+++ 06LINQ/ZTM.DotNet/LinqIntroduction/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Data source
+var numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+
+// 2. Query creation
+var evenNumbers = numbers.Where(FilterEvenNumbers);
+
+numbers.Add(7);
+numbers.Add(8);
+
+// 3. Query execution
+foreach (int num in evenNumbers)
+{
+    Console.Write("{0,1} ", num);
+}
+
+static bool FilterEvenNumbers(int num)
+{
+    return num % 2 == 0;
+}
