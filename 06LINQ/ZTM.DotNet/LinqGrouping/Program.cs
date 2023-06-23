45c2bcea4f24b1fdd5384f5b2abfc603d5eb0a92
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-22 18:58:17 -0700

    Completed the GroupBy lesson.

---
+++ 06LINQ/ZTM.DotNet/LinqGrouping/Program.cs
@@ -0,0 +1,22 @@
+﻿var employees = new List<Employee>
+{
+    new Employee("John", "Miller", "Development"),
+    new Employee("Jack", "Stevens", "Sales"),
+    new Employee("Maria", "McAllister", "Sales"),
+    new Employee("Christopher", "Collum", "Project Management"),
+    new Employee("Javier", "Alegro", "Development")
+};
+
+var personsByDepartment = employees.GroupBy(person => person.Department);
+
+foreach (var department in personsByDepartment)
+{
+    Console.WriteLine($"Department: {department.Key}");
+
+    foreach (var employee in department)
+    {
+        Console.WriteLine($"    {employee.FirstName} {employee.LastName}");
+    }
+}
+
+record Employee(string FirstName, string LastName, string Department);
