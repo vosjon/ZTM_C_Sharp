ec6a029e69699f758f53ea2f5fe4633aab445500
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-22 18:49:24 -0700

    Completed the LINQ select lesson.

---
+++ 06LINQ/ZTM.DotNet/LinqSelecting/Program.cs
@@ -0,0 +1,35 @@
+﻿var persons = new List<Person>
+{
+    new Person("Jack", "Miller", 27),
+    new Person("Jack", "Stevens", 41),
+    new Person("Maria", "Mc Allister", 29),
+    new Person("Christopher", "Collum", 36),
+    new Person("Javier", "Alegro", 34)
+};
+
+var lastNames = persons.Select(person => person.LastName);
+foreach (var lastName in lastNames)
+{
+    Console.WriteLine(lastName);
+}
+
+var ageBetween20and30 = persons
+    .Where(person => person.Age >= 20 && person.Age <= 30)
+    .Select(person => person.Age);
+
+foreach (var age in ageBetween20and30)
+{
+    Console.WriteLine(age);
+}
+
+Console.WriteLine();
+
+var firstPerson = persons.FirstOrDefault();
+Console.WriteLine(firstPerson.FirstName);
+
+foreach (var person in persons.Skip(1).Take(2))
+{
+    Console.WriteLine(person);
+}
+
+record Person(string FirstName, string LastName, int Age);
