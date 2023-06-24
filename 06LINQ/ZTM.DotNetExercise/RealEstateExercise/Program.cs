f5f607052ba7469d3e1dae974badaf4d894ce4c1
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-24 09:54:58 -0700

    Completed the exercise for the LINQ section of the course.

--- 06LINQ/ZTM.DotNetExercise/RealEstateExercise/Program.cs
+++ 06LINQ/ZTM.DotNetExercise/RealEstateExercise/Program.cs
@@ -28,4 +28,21 @@ foreach (var house in largeHouses)
     Console.WriteLine(house);
 }
 
+// FirstOrDefault
+var firstHouse = familyHomes.FirstOrDefault();
+Console.WriteLine($"\nThe first home in the list is: {firstHouse.Name}");
+
+// Orderby sqrfeet
+Console.WriteLine("\nHouses ordered by squarefeet:");
+foreach (var house in familyHomes.OrderBy(house => house.SquareFeet))
+{
+    Console.WriteLine($"{house.Name} - Squarefeet: {house.SquareFeet}");
+}
+
+Console.WriteLine("\nHouses ordered by squarefeet descending:");
+foreach (var house in familyHomes.OrderByDescending(house => house.SquareFeet))
+{
+    Console.WriteLine($"{house.Name} - Squarefeet: {house.SquareFeet}");
+}
+
 public record House(decimal SquareFeet, decimal TaxPerSize, string Name);
