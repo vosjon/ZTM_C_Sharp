784b41b555f70b6d82d58517e7306c36a87a926c
Jonathan Vos <vosjonjoe@gmail.com>
2023-06-23 18:14:39 -0700

    Completed all lessons from the LINQ section.

---
+++ 06LINQ/ZTM.DotNet/LinqQuerySyntax/Program.cs
@@ -0,0 +1,35 @@
+﻿var employees = new List<Employee>
+{
+    new Employee("John", "Miller", "Development"),
+    new Employee("Jack", "Stevens", "Sales"),
+    new Employee("Maria", "McAllister", "Sales"),
+    new Employee("Christopher", "Collum", "Project Management"),
+    new Employee("Javier", "Alegro", "Development")
+};
+
+var selectedEmployees = from employee in employees
+                        where employee.FirstName.StartsWith("J")
+                        orderby employee.LastName
+                        select employee;
+
+foreach (var employee in selectedEmployees)
+{
+    Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+}
+
+var employeesByDepartment = from employee in employees
+                            group employee by employee.Department into groupedEmployees
+                            orderby groupedEmployees.Key
+                            select groupedEmployees;
+
+foreach (var department in employeesByDepartment)
+{
+    Console.WriteLine($"Department: {department.Key}");
+
+    foreach (var employee in department)
+    {
+        Console.WriteLine($"    {employee.FirstName} {employee.LastName}");
+    }
+}
+
+record Employee(string FirstName, string LastName, string Department);
